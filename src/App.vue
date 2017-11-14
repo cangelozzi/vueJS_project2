81e6b5986260898d86803204a9025d2be16c3fd0
camgelozzi <cangelozzi@gmail.com>
2017-11-14 17:28:38 -0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,53 @@
+<template>
+    <div class="container">
+      <appHeader :quoteCount="quotes.length" :maxQuotes="maxQuotes"></appHeader>
+      <appNewQuote @quoteAdded="newQuote"></appNewQuote>
+      <appQuoteGrid :quotes="quotes" @quoteDeleted="deleteQuote"></appQuoteGrid>
+
+    <p>Info: Click on a Quote to Delete it!</p>
+    </div>
+</template>
+
+<script>
+  import Header from './components/Header.vue';
+  import Quote from './components/Quote.vue';
+  import QuoteGrid from './components/QuoteGrid.vue';
+  import NewQuote from './components/NewQuote.vue';
+
+  export default {
+    data: function(){
+      return {
+        maxQuotes: 10,
+        quotes: ['First Quote']
+      }
+    },
+    components: {
+      appHeader: Header,
+      appQuote: Quote,
+      appQuoteGrid: QuoteGrid,
+      appNewQuote: NewQuote
+    },
+
+    methods: {
+      newQuote(quote) {
+        if(this.quotes.length >= this.maxQuotes) {
+          return alert('Please delete quote first');
+        }
+        this.quotes.push(quote);
+
+      },
+      deleteQuote(index) {
+        this.quotes.splice(index,1);
+      }
+    }
+
+    }
+</script>
+
+<style>
+p {
+  background-color: yellow;
+  text-align: center;
+  padding: 20px;
+}
+</style>
---
+++ src/components/Quote.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="col-sm6 col-md4 col-lg-3">
+    <div class="panel panel-default">
+      <div class="panel-body quote">
+        <slot></slot>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style>
+  .panel-body {
+    font-family: 'Arizona', cursive;
+    font-size: 24px;
+    color: #6e6e6e
+  }
+
+  .quote {
+    cursor: pointer;
+  }
+
+  .quote:hover {
+    background-color: #ffe2e2;
+  }
+</style>
